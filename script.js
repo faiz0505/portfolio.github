fe90f6d8dd3a2f70870ce40bdcc8fe18eeeea77f
faiz0505 <110318645+faiz0505@users.noreply.github.com>
2023-02-06 17:54:20 +0530

    test

---
+++ script.js
@@ -0,0 +1,20 @@
+
+	var tablinks = document.getElementsByClassName('tab-links');
+	var tabcontents = document.getElementsByClassName('tab-contents');
+	function opentab(tabname){
+	for(tablink of tablinks){
+	   tablink.classList.remove('active-link');
+	}
+	for(tabcontent of tabcontents){
+	   tabcontent.classList.remove('active-tab');
+	}
+	event.currentTarget.classList.add('active-link');
+	document.getElementById(tabname).classList.add('active-tab');
+	}
+	
+	function openmenu(){
+		document.getElementById('sidemenu').style.right = '0';
+	}
+	function closemenu(){
+		document.getElementById('sidemenu').style.right = '-200px';
+	}
